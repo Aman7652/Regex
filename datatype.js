28bac1a89895ed31689a2abc8dc177affe4fc0d4
Aman7652 <160305799+Aman7652@users.noreply.github.com>
2024-12-01 14:20:44 +0530

    second commit

---
+++ datatype.js
@@ -0,0 +1,7 @@
+let name= "Aman";
+let age= 25;
+let city= "Delhi";
+ const pi= 3.14;
+ var A="A";
+ 
+console.log(city);
---
+++ function.js
@@ -0,0 +1,13 @@
+// function func(a){
+//     console.log(a);
+
+// }
+// let a =4;
+// func(a);
+
+function sum(a,b){
+    return a+b;
+}
+let b = 5;
+let a= 2;
+console.log("SUM:",sum(a,b));
